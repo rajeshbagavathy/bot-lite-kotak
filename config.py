9f82d8c6f49867202e06d683641e1f8c6bc021f5
Rajes kumar bagavathy <rajeshbagavathy@gmail.com>
2026-03-02 09:13:03 +0530

    strategy renmae

--- config.py
+++ config.py
@@ -58,12 +58,12 @@ INDEX_CONFIGS = {
 }
 
 STRATEGIES = [
-    StrategyConfig("S0921", "09:30:00", 7, 20.0, 10000.0),
-    StrategyConfig("S0955", "09:50:00", 7, 30.0, 10000.0),
+    StrategyConfig("S0930", "09:30:00", 7, 20.0, 10000.0),
+    StrategyConfig("S0950", "09:50:00", 7, 30.0, 10000.0),
     StrategyConfig("S1005", "10:05:00", 7, 30.0, 10000.0),
-    StrategyConfig("S1005", "10:25:00", 7, 30.0, 10000.0),
+    StrategyConfig("S1025", "10:25:00", 7, 30.0, 10000.0),
     StrategyConfig("S1045", "10:45:00", 7, 35.0, 10000.0),
-    StrategyConfig("S1205", "12:55:00", 7, 35.0, 10000.0),
+    StrategyConfig("S1255", "12:55:00", 7, 35.0, 10000.0),
 ]
 
 PORTFOLIO_SL_LIMIT = -50000.0
