0f3cbe361cdcb9e7592ccc3dcc1990ab3919a6af
Rajes kumar bagavathy <rajeshbagavathy@gmail.com>
2026-03-10 10:22:37 +0530

    changed sl percent

--- config.py
+++ config.py
@@ -59,12 +59,12 @@ INDEX_CONFIGS = {
 
 STRATEGIES = [
     StrategyConfig("S0930", "09:30:00", 7, 20.0, 10000.0),
-    StrategyConfig("S0950", "09:50:00", 7, 30.0, 10000.0),
-    StrategyConfig("S1005", "10:05:00", 7, 30.0, 10000.0),
-    StrategyConfig("S1025", "10:25:00", 7, 30.0, 10000.0),
-    StrategyConfig("S1045", "10:45:00", 7, 35.0, 10000.0),
-    StrategyConfig("S1144", "11:44:00", 7, 35.0, 10000.0),
-    StrategyConfig("S1255", "12:55:00", 7, 35.0, 10000.0),
+    StrategyConfig("S0950", "09:50:00", 7, 20.0, 10000.0),
+    StrategyConfig("S1005", "10:05:00", 7, 20.0, 10000.0),
+    StrategyConfig("S1025", "10:25:00", 7, 20.0, 10000.0),
+    StrategyConfig("S1045", "10:45:00", 7, 20.0, 10000.0),
+    StrategyConfig("S1144", "11:44:00", 7, 20.0, 10000.0),
+    StrategyConfig("S1255", "12:55:00", 7, 20.0, 10000.0),
 ]
 
 PORTFOLIO_SL_LIMIT = -80000.0
